35cd4d90bd0f93e53d80f46ef15b83382826a58f
Caio Paulino <10paulinhosilva10@gmail.com>
2024-04-26 14:14:32 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,10 +1,17 @@
 function gerarTabuada() {
     var num = document.getElementById('num')
-    var n = Number(num.value)
-    var res = document.getElementById('res')
-    for(let i=1; i<=10; i++) {
-        var aux = n*i
-        res.innerHTML += `<br>${i}`+ " X " + `${n}`+ " = " + `${aux}<br>` 
+    var tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert("Por favor, digite um número!")
+    } else {
+        var n = Number(num.value)
     }
-    
+    for(let i=1; i<=10; i++) {
+        let item = document.createElement('option')
+        var aux = n*i
+        item.text = `${i}`+ " X " + `${n}`+ " = " + `${aux}`
+        tab.appendChild(item)
+        // res.innerHTML += `<br>${i}`+ " X " + `${n}`+ " = " + `${aux}<br>` 
+    }
+
 }
